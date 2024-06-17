f18f31fcd8e11d5fa31a4b86937e6a8dead8bcf5
calebgward <calebgward@gmail.com>
2024-06-17 02:26:06 +0000

    Working valid_move function

--- src/board.py
+++ src/board.py
@@ -6,21 +6,27 @@ class Board:
         self.init_board()
 
     def init_board(self):
-            self.board[0][2] = Tile([0,2,0,0])
-            self.board[0][4] = Tile([0,1,0,0])
-            self.board[0][6] = Tile([0,2,0,0])
-            
-            self.board[8][2] = Tile([0,0,0,2])
-            self.board[8][4] = Tile([0,0,0,1])
-            self.board[8][6] = Tile([0,0,0,2])
+        for i in range(9):
+            self.board[i][0] = Tile([0,0,0,0])
+            self.board[i][8] = Tile([0,0,0,0])
+            self.board[0][i] = Tile([0,0,0,0])
+            self.board[8][i] = Tile([0,0,0,0])
 
-            self.board[2][0] = Tile([0,0,1,0])
-            self.board[4][0] = Tile([0,0,2,0])
-            self.board[6][0] = Tile([0,0,1,0])
+        self.board[0][2] = Tile([0,2,0,0])
+        self.board[0][4] = Tile([0,1,0,0])
+        self.board[0][6] = Tile([0,2,0,0])
+        
+        self.board[8][2] = Tile([0,0,0,2])
+        self.board[8][4] = Tile([0,0,0,1])
+        self.board[8][6] = Tile([0,0,0,2])
 
-            self.board[2][8] = Tile([1,0,0,0])
-            self.board[4][8] = Tile([2,0,0,0])
-            self.board[6][8] = Tile([1,0,0,0])
+        self.board[2][0] = Tile([0,0,1,0])
+        self.board[4][0] = Tile([0,0,2,0])
+        self.board[6][0] = Tile([0,0,1,0])
+
+        self.board[2][8] = Tile([1,0,0,0])
+        self.board[4][8] = Tile([2,0,0,0])
+        self.board[6][8] = Tile([1,0,0,0])
 
     def place_tile(self, tile, x, y):
         if self.valid_move(tile, x, y):
@@ -33,13 +39,17 @@ class Board:
         exit_rotation = 0
         for dx, dy in [(0,-1),(-1,0),(0,1),(1,0)]:
             if self.get_tile(x+dx, y+dy) is None:
+                exit_rotation += 1
                 continue
-
-            print(tile.get_exits())
-            print(self.get_tile(x+dx, y+dy).get_exits())
-            if tile.get_exits()[exit_rotation] == self.get_tile(x+dx, y+dy).get_exits()[(exit_rotation+2)%4]:
-                is_valid = True
-                break
+            
+            print(tile.get_exits()[(exit_rotation+2)%4])
+            print(self.get_tile(x+dx, y+dy).get_exits()[(exit_rotation)%4])
+            if tile.get_exits()[(exit_rotation+2)%4] != 0 and self.get_tile(x+dx, y+dy).get_exits()[exit_rotation] != 0:
+                if tile.get_exits()[(exit_rotation+2)%4] != self.get_tile(x+dx, y+dy).get_exits()[exit_rotation]:
+                    return False
+                else:
+                    is_valid = True
+                
             exit_rotation += 1
                     
         return is_valid
--- src/main.py
+++ src/main.py
@@ -6,6 +6,6 @@ class Game:
         self.board = Board()
 
 game = Game()
-new_tile = Tile([2,2,2,2])
+new_tile = Tile([0,1,0,2])
 print(game.board.place_tile(new_tile, 1, 2))
 print(game.board.place_tile(new_tile, 2, 2))
